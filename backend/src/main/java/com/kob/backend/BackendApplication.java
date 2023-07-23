fab1409991bb38dd6a6b3e77a121ec9330d87974
dengzuxuan <dengzuxuan20010404@126.com>
2023-07-24 02:28:24 +0800

    将gradle改为maven v0724

--- backend/src/main/java/com/kob/backend/BackendApplication.java
+++ backend/src/main/java/com/kob/backend/BackendApplication.java
@@ -3,12 +3,11 @@ package com.kob.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
+        System.out.println(1);
         SpringApplication.run(BackendApplication.class, args);
     }
 
